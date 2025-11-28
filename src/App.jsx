7aff209e4f087c7efc9f568b5c83a54ee1260db2
vlad133L <vlad133L@gmail.com>
2025-11-28 20:02:54 +0300

    added gameboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import GameBoard from './components/GameBoard';
+import Player from './components/Player';
+
 function App() {
-  return <h1></h1>;
+  return (
+    <main>
+      <div id='game-container'>
+        <ol id='players'>
+          <Player initialName='Player 1' symbol='X' />
+          <Player initialName='Player 2' symbol='O' />
+        </ol>
+        <GameBoard />
+      </div>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+const Player = ({ initialName, symbol }) => {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditClick() {
+    setIsEditing((prev) => !prev);
+  }
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+  let editablePlayerName = <span className='player-name'>{playerName}</span>;
+  if (isEditing) {
+    editablePlayerName = (
+      <input type='text' required value={playerName} onChange={handleChange} />
+    );
+  }
+  return (
+    <li>
+      <span className='player'>
+        {editablePlayerName}
+        <span className='player-name'>{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  );
+};
+
+export default Player;
